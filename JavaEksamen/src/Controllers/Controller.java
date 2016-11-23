2172d1bea12527f77212c9ff6efb13f1cb98f7bd
EmilEhrenreich <emil.ehrenreich@gmail.com>
2016-11-23 16:50:32 +0100

    Commit 2 - Tjek af alt kode kommer op

--- JavaEksamen/src/Controllers/Controller.java
+++ JavaEksamen/src/Controllers/Controller.java
@@ -1,7 +1,39 @@
 package Controllers;
 
+import MenuView.MainMenu;
+import MenuView.UserMenu;
+
 /**
  * Created by EmilEhrenreich on 22/11/2016.
  */
 public class Controller {
+
+    private MainMenu mainMenu;
+    private BookMenu bookMenu;
+    private UserMenu userMenu;
+
+
+    public Controller(){
+        this.mainMenu = new MainMenu(this);
+        this.bookMenu = new BookMenu(this);
+        this.userMenu = new UserMenu(this);
+    }
+
+
+    //Hvorfor laver man {} rundt om nedenstående del?
+    public void ShowMainMenu() { this.mainMenu.presentMenu(); }
+
+
+    public MainMenu getMainMenu() {
+        return mainMenu;
+    }
+
+    public BookMenu getBookMenu() {
+        return bookMenu;
+    }
+
+    public UserMenu getUserMenu() {
+        return userMenu;
+    }
 }
+
